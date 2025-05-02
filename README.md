319b4f15cb1f5689a3eb67c589cba968ec9a9062
luna <luna@specialmomentsgroup.com>
2025-05-02 11:18:46 +0200

    try cursor party

--- README.md
+++ README.md
@@ -1,2 +1,5 @@
 # Portfolio-new
+
 This is my portfolio update 2025
+
+trying this when home -> https://github.com/partykit/cursor-party
--- main.js
+++ main.js
@@ -1,17 +1,54 @@
-const Socket = new WebSocket('ws://localhost:8000');
-let color = Math.floor(Math.random() * 7);
-let cursor = document.createElement('div');
-document.body.append(cursor);
+function generateRandomString() {
+	const characters = 'ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789';
+	let result = '';
+	for (let i = 0; i < 10; i++) {
+		const randomIndex = Math.floor(Math.random() * characters.length);
+		result += characters.charAt(randomIndex);
+	}
+	return result;
+}
 
-cursor.classList.add('cursor');
-cursor.style.backgroundImage = 'url(\'img/cursor' + color + '.png\')';
+let ID = generateRandomString();
+console.log(ID);
+
+function createcursor(a) {
+	let cursorId = a;
+	let color = Math.floor(Math.random() * 7);
+	let cursor = document.createElement('div');
+	document.body.append(cursor);
+	cursor.classList.add('cursor');
+	cursor.id = cursorId;
+	cursor.style.backgroundImage = 'url(\'img/cursor' + color + '.png\')';
+
+	return cursor;
+}
 
 let x = 0;
+
 let y = 0;
+UserCursor = createcursor(ID);
+
+if (UserCursor.id == ID) {
+	UserCursor.style.display = 'none';
+}
+
+window.addEventListener('scroll', (e) => {
+	x = e.pageX;
+	y = e.pageY;
+	UserCursor.style.left = e.pageX + 'px';
+	UserCursor.style.top = e.pageY + 'px';
+});
 
 window.addEventListener('mousemove', (e) => {
 	x = e.pageX;
 	y = e.pageY;
-	cursor.style.left = e.pageX + 'px';
-	cursor.style.top = e.pageY + 'px';
+	UserCursor.style.left = e.pageX + 'px';
+	UserCursor.style.top = e.pageY + 'px';
 });
+
+
+const socket = new WebSocket('ws://localhost:8000');
+socket.addEventListener('open', (event) => {
+	socket.send('Hello Server!');
+});
+
